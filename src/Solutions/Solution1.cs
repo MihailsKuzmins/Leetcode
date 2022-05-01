2fd058f8e831b89e290583bfc6339208f2b41d97
ミシャ <47413092+MihailsKuzmins@users.noreply.github.com>
2022-05-01 15:41:38 +0900

    solution1

---
+++ src/Solutions/Solution1.cs
@@ -0,0 +1,19 @@
+﻿namespace MyNihongo.Leetcode.Solutions;
+
+public sealed class Solution1
+{
+	public int[] TwoSum(int[] nums, int target)
+	{
+		var dictionary = new Dictionary<int, int>();
+		for (var i = 0; i < nums.Length; i++)
+		{
+			var result = target - nums[i];
+			if (dictionary.TryGetValue(result, out var value))
+				return new[] { value, i };
+
+			dictionary.Add(nums[i], i);
+		}
+
+		throw new Exception();
+	}
+}
