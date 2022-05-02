9f89c8d571f3a1ac86e7036dc8cd54533fef986a
ミシャ <47413092+MihailsKuzmins@users.noreply.github.com>
2022-05-02 22:18:18 +0900

    solution905

---
+++ src/Solutions/Solution905.cs
@@ -0,0 +1,32 @@
+﻿namespace MyNihongo.Leetcode.Solutions;
+
+public sealed class Solution905
+{
+	public static int[] SortArrayByParity(int[] nums)
+	{
+		for (var i = 0; i < nums.Length; i++)
+		{
+			if (nums[i] % 2 == 0)
+				continue;
+
+			var isMoved = false;
+			for (var j = i + 1; j < nums.Length; j++)
+			{
+				if (nums[j] % 2 != 0)
+					continue;
+
+				nums[i] += nums[j];
+				nums[j] = nums[i] - nums[j];
+				nums[i] -= nums[j];
+
+				isMoved = true;
+				break;
+			}
+
+			if (!isMoved)
+				break;
+		}
+
+		return nums;
+	}
+}
---
+++ tests/MyNihongo.Leetcode.Tests/Solutions/Solution905Should.cs
@@ -0,0 +1,30 @@
+﻿namespace MyNihongo.Leetcode.Tests.Solutions;
+
+public sealed class Solution905Should
+{
+	[Fact]
+	public void Case1()
+	{
+		var input = new[] { 3, 1, 2, 4 };
+		var expected = new[] { 2, 4, 3, 1 };
+
+		var result = Solution905.SortArrayByParity(input);
+
+		result
+			.Should()
+			.BeEquivalentTo(expected, x => x.WithStrictOrdering());
+	}
+
+	[Fact]
+	public void Case2()
+	{
+		var input = new[] { 0 };
+		var expected = new[] { 0 };
+
+		var result = Solution905.SortArrayByParity(input);
+
+		result
+			.Should()
+			.BeEquivalentTo(expected, x => x.WithStrictOrdering());
+	}
+}
