f3debce28cd133cfc6b5825c469f0964e5152f5f
ミシャ <47413092+MihailsKuzmins@users.noreply.github.com>
2022-05-06 08:18:26 +0900

    solution27

---
+++ tests/MyNihongo.Leetcode.Tests/Solutions/Solution27Should.cs
@@ -0,0 +1,36 @@
+﻿namespace MyNihongo.Leetcode.Tests.Solutions;
+
+public sealed class Solution27Should
+{
+	[Fact]
+	public void Case1()
+	{
+		var nums = new[] { 3, 2, 2, 3 };
+		const int val = 3;
+
+		var expected = new[] { 2, 2 };
+
+		var length = Solution27.RemoveElement(nums, val);
+		var result = nums[..length];
+
+		result
+			.Should()
+			.BeEquivalentTo(expected);
+	}
+
+	[Fact]
+	public void Case2()
+	{
+		var nums = new[] { 0, 1, 2, 2, 3, 0, 4, 2 };
+		const int val = 2;
+
+		var expected = new[] { 0, 1, 4, 0, 3 };
+
+		var length = Solution27.RemoveElement(nums, val);
+		var result = nums[..length];
+
+		result
+			.Should()
+			.BeEquivalentTo(expected);
+	}
+}
