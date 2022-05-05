b8866e7603e148c6a486bb6b37e9ce52018ec14c
ミシャ <47413092+MihailsKuzmins@users.noreply.github.com>
2022-05-05 10:00:56 +0900

    solution26

---
+++ tests/MyNihongo.Leetcode.Tests/Solutions/Solution26Should.cs
@@ -0,0 +1,32 @@
+﻿namespace MyNihongo.Leetcode.Tests.Solutions;
+
+public sealed class Solution26Should
+{
+	[Fact]
+	public void Case1()
+	{
+		int[] input = { 1, 1, 2 },
+			expected = { 1, 2 };
+
+		var index = Solution26.RemoveDuplicates(input);
+		var result = input[..index];
+
+		result
+			.Should()
+			.BeEquivalentTo(expected, x => x.WithStrictOrdering());
+	}
+
+	[Fact]
+	public void Case2()
+	{
+		int[] input = { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 },
+			expected = { 0, 1, 2, 3, 4 };
+
+		var index = Solution26.RemoveDuplicates(input);
+		var result = input[..index];
+
+		result
+			.Should()
+			.BeEquivalentTo(expected, x => x.WithStrictOrdering());
+	}
+}
