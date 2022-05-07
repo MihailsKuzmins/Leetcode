d0c6f30c9e38a0f6382cad1df33db54d97a749e8
ミシャ <47413092+MihailsKuzmins@users.noreply.github.com>
2022-05-07 22:04:32 +0900

    solution69

---
+++ tests/MyNihongo.Leetcode.Tests/Solutions/Solution69Should.cs
@@ -0,0 +1,43 @@
+﻿namespace MyNihongo.Leetcode.Tests.Solutions;
+
+public sealed class Solution69Should
+{
+	[Fact]
+	public void Case1()
+	{
+		const int input = 4,
+			expected = 2;
+
+		var result = Solution69.MySqrt(input);
+
+		result
+			.Should()
+			.Be(expected);
+	}
+
+	[Fact]
+	public void Case2()
+	{
+		const int input = 8,
+			expected = 2;
+
+		var result = Solution69.MySqrt(input);
+
+		result
+			.Should()
+			.Be(expected);
+	}
+
+	[Fact]
+	public void Case3()
+	{
+		const int input = 2147395599,
+			expected = 46339;
+
+		var result = Solution69.MySqrt(input);
+
+		result
+			.Should()
+			.Be(expected);
+	}
+}
