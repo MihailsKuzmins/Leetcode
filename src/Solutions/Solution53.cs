2fa5c66caeabfc65dd069228613b40cf8250eb2d
ミシャ <47413092+MihailsKuzmins@users.noreply.github.com>
2022-05-07 11:10:13 +0900

    solution53

---
+++ src/Solutions/Solution53.cs
@@ -0,0 +1,34 @@
+﻿namespace MyNihongo.Leetcode.Solutions;
+
+/// <summary>
+/// Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.<br/>
+/// A <b>subarray</b> is a <b>contiguous</b> part of an array.
+/// </summary>
+public sealed class Solution53
+{
+	public static int MaxSubArray(int[] nums)
+	{
+		if (nums.Length == 0)
+			return 0;
+
+		var max = nums[0];
+		for (int i = 1, sum = max; i < nums.Length; i++)
+		{
+			// Trick: if the sum has become negative there is no way that it can become the max sum
+			// E.g. If we hit negative and the next number is greater, the next number itself is greater than the sum
+			// Therefore, we need to discard all the input as soon as we hit negative
+
+			// If the sum becomes negative we don't need calculate all the previous elements
+			// So we clear the accumulated sum and start from the current element
+			if (sum < 0)
+				sum = 0;
+
+			sum += nums[i];
+
+			if (sum > max)
+				max = sum;
+		}
+
+		return max;
+	}
+}
---
+++ tests/MyNihongo.Leetcode.Tests/Solutions/Solution53Should.cs
@@ -0,0 +1,56 @@
+﻿namespace MyNihongo.Leetcode.Tests.Solutions;
+
+public sealed class Solution53Should
+{
+	[Fact]
+	public void Case1()
+	{
+		var nums = new[] { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
+		const int expected = 6;
+
+		var result = Solution53.MaxSubArray(nums);
+
+		result
+			.Should()
+			.Be(expected);
+	}
+
+	[Fact]
+	public void Case2()
+	{
+		var nums = new[] { 1 };
+		const int expected = 1;
+
+		var result = Solution53.MaxSubArray(nums);
+
+		result
+			.Should()
+			.Be(expected);
+	}
+
+	[Fact]
+	public void Case3()
+	{
+		var nums = new[] { 5, 4, -1, 7, 8 };
+		const int expected = 23;
+
+		var result = Solution53.MaxSubArray(nums);
+
+		result
+			.Should()
+			.Be(expected);
+	}
+
+	[Fact]
+	public void Case4()
+	{
+		var nums = new[] { -5, -3 };
+		const int expected = 23;
+
+		var result = Solution53.MaxSubArray(nums);
+
+		result
+			.Should()
+			.Be(expected);
+	}
+}
