4a2e367d9e515be110770286c9c7739e5361771c
ミシャ <47413092+MihailsKuzmins@users.noreply.github.com>
2022-05-08 12:14:56 +0900

    solution70

---
+++ tests/MyNihongo.Leetcode.Tests/Solutions/Solution70Should.cs
@@ -0,0 +1,30 @@
+﻿namespace MyNihongo.Leetcode.Tests.Solutions;
+
+public sealed class Solution70Should
+{
+	[Fact]
+	public void Case1()
+	{
+		const int input = 2,
+			expected = 2;
+
+		var result = Solution70.ClimbStairs(input);
+
+		result
+			.Should()
+			.Be(expected);
+	}
+
+	[Fact]
+	public void Case2()
+	{
+		const int input = 3,
+			expected = 3;
+
+		var result = Solution70.ClimbStairs(input);
+
+		result
+			.Should()
+			.Be(expected);
+	}
+}
