25b69abfeaea6b5d566ad6e9b2e7d62cff9d9e52
ミシャ <47413092+MihailsKuzmins@users.noreply.github.com>
2022-05-08 12:47:16 +0900

    solution83

---
+++ src/Solutions/Solution83.cs
@@ -0,0 +1,55 @@
+﻿// ReSharper disable InconsistentNaming
+namespace MyNihongo.Leetcode.Solutions;
+
+/// <summary>
+/// Given the head of a sorted linked list, delete all duplicates such that each element appears only once. Return the linked list <b>sorted</b> as well.
+/// </summary>
+public sealed class Solution83
+{
+	public static ListNode? DeleteDuplicates(ListNode? head)
+	{
+		if (head == null)
+			return null;
+
+		var unique = head;
+		var curr = unique.next;
+		var num = unique.val;
+		while (curr != null)
+		{
+			var isUnique = curr.val != num;
+			if (isUnique)
+			{
+				unique = unique.next = curr;
+				num = curr.val;
+			}
+
+			curr = curr.next;
+
+			if (!isUnique)
+				unique.next = null;
+		}
+
+		return head;
+	}
+
+	public sealed class ListNode
+	{
+		public int val;
+		public ListNode? next;
+
+		public ListNode(int val = 0, ListNode? next = null)
+		{
+			this.val = val;
+			this.next = next;
+		}
+
+		public override string ToString()
+		{
+			var str = val.ToString();
+			if (next != null)
+				str += " -> ";
+
+			return str;
+		}
+	}
+}
---
+++ tests/MyNihongo.Leetcode.Tests/Solutions/Solution83Should.cs
@@ -0,0 +1,55 @@
+﻿namespace MyNihongo.Leetcode.Tests.Solutions;
+
+public sealed class Solution83Should
+{
+	[Fact]
+	public void Case1()
+	{
+		Solution83.ListNode item3 = new(2), item2 = new(1, item3), item1 = new(1, item2);
+		var expected = new[] { item1, item3 };
+
+		var node = Solution83.DeleteDuplicates(item1);
+		var result = CreateList(node);
+
+		result
+			.Should()
+			.BeEquivalentTo(expected, x => x.WithStrictOrdering().IncludingFields());
+	}
+
+	[Fact]
+	public void Case2()
+	{
+		Solution83.ListNode item5 = new(3), item4 = new(3, item5), item3 = new(2, item4), item2 = new(1, item3), item1 = new(1, item2);
+		var expected = new[] { item1, item3, item5 };
+
+		var node = Solution83.DeleteDuplicates(item1);
+		var result = CreateList(node);
+
+		result
+			.Should()
+			.BeEquivalentTo(expected, x => x.WithStrictOrdering().IncludingFields());
+	}
+
+	[Fact]
+	public void Case3()
+	{
+		var node = Solution83.DeleteDuplicates(null);
+		var result = CreateList(node);
+
+		result
+			.Should()
+			.BeEmpty();
+	}
+
+	private static IEnumerable<Solution83.ListNode> CreateList(Solution83.ListNode? node)
+	{
+		var list = new List<Solution83.ListNode>();
+		while (node != null)
+		{
+			list.Add(node);
+			node = node.next;
+		}
+
+		return list;
+	}
+}
