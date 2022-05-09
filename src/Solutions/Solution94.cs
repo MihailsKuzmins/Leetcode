8c03549d7eb1643f7256570ae0d3af7b80ed0687
ミシャ <47413092+MihailsKuzmins@users.noreply.github.com>
2022-05-09 14:43:03 +0900

    solution94

---
+++ src/Solutions/Solution94.cs
@@ -0,0 +1,49 @@
+﻿// ReSharper disable InconsistentNaming
+namespace MyNihongo.Leetcode.Solutions;
+
+/// <summary>
+/// Given the root of a binary tree, return the inorder traversal of its nodes' values.
+/// </summary>
+public sealed class Solution94
+{
+	public static IList<int> InorderTraversal(TreeNode? root)
+	{
+		if (root == null)
+			return Array.Empty<int>();
+
+		var list = new List<int>();
+		var stack = new Stack<TreeNode>();
+
+		while (root != null || stack.Count > 0)
+		{
+			// Reach the left
+			while (root != null)
+			{
+				stack.Push(root);
+				root = root.left;
+			}
+
+			root = stack.Pop();
+			list.Add(root.val);
+
+			// Reach the right
+			root = root.right;
+		}
+
+		return list;
+	}
+
+	public sealed class TreeNode
+	{
+		public int val;
+		public TreeNode? left;
+		public TreeNode? right;
+
+		public TreeNode(int val = 0, TreeNode? left = null, TreeNode? right = null)
+		{
+			this.val = val;
+			this.left = left;
+			this.right = right;
+		}
+	}
+}
---
+++ tests/MyNihongo.Leetcode.Tests/Solutions/Solution94Should.cs
@@ -0,0 +1,40 @@
+﻿namespace MyNihongo.Leetcode.Tests.Solutions;
+
+public sealed class Solution94Should
+{
+	[Fact]
+	public void Case1()
+	{
+		Solution94.TreeNode item3 = new(3), item2 = new(2, item3), item1 = new(1, right: item2);
+		var expected = new [] { 1, 3, 2 };
+
+		var result = Solution94.InorderTraversal(item1);
+
+		result
+			.Should()
+			.BeEquivalentTo(expected, x => x.WithStrictOrdering());
+	}
+
+	[Fact]
+	public void Case2()
+	{
+		var result = Solution94.InorderTraversal(null);
+
+		result
+			.Should()
+			.BeEmpty();
+	}
+
+	[Fact]
+	public void Case3()
+	{
+		Solution94.TreeNode item1 = new(1);
+		var expected = new[] { 1 };
+
+		var result = Solution94.InorderTraversal(item1);
+
+		result
+			.Should()
+			.BeEquivalentTo(expected, x => x.WithStrictOrdering());
+	}
+}
