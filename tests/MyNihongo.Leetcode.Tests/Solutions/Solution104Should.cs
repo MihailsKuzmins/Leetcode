30a6321d80846a488c5d6a732411d14502520788
ミシャ <47413092+MihailsKuzmins@users.noreply.github.com>
2022-05-13 09:07:38 +0900

    solution104

---
+++ tests/MyNihongo.Leetcode.Tests/Solutions/Solution104Should.cs
@@ -0,0 +1,35 @@
+﻿namespace MyNihongo.Leetcode.Tests.Solutions;
+
+public sealed class Solution104Should
+{
+	[Fact]
+	public void Case1()
+	{
+		Solution104.TreeNode left = new(9),
+			right1 = new(15), right2 = new(7), right = new(20, right1, right2),
+			item = new(3, left, right);
+
+		const int expected = 3;
+
+		var result = Solution104.MaxDepth(item);
+
+		result
+			.Should()
+			.Be(expected);
+	}
+
+	[Fact]
+	public void Case2()
+	{
+		Solution104.TreeNode right = new(2),
+			item = new(1, right: right);
+
+		const int expected = 2;
+
+		var result = Solution104.MaxDepth(item);
+
+		result
+			.Should()
+			.Be(expected);
+	}
+}
