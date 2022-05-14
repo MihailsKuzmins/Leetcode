88a0c9795bf2d1f72fe47fcec10801bfb5e41e35
ミシャ <47413092+MihailsKuzmins@users.noreply.github.com>
2022-05-14 21:46:30 +0900

    solution108

---
+++ src/Solutions/Solution108.cs
@@ -0,0 +1,52 @@
+﻿// ReSharper disable InconsistentNaming
+namespace MyNihongo.Leetcode.Solutions;
+
+/// <summary>
+/// Given an integer array nums where the elements are sorted in ascending order, convert it to a height-balanced binary search tree.<br/>
+/// A <b>height-balanced</b> binary tree is a binary tree in which the depth of the two subtrees of every node never differs by more than one.
+/// </summary>
+public sealed class Solution108
+{
+	// TODO: try iterative
+	public static TreeNode? SortedArrayToBST(int[] nums)
+	{
+		return nums.Length != 0
+			? GetNode(nums, 0, nums.Length - 1)
+			: null;
+
+		static TreeNode? GetNode(in int[] nums, in int start, in int end)
+		{
+			if (start > end)
+				return null;
+
+			var med = (start + end) / 2;
+			var right = GetNode(nums, med + 1, end);
+			var left = GetNode(nums, start, med - 1);
+
+			return new TreeNode(nums[med])
+			{
+				right = right,
+				left = left
+			};
+		}
+	}
+
+	public sealed class TreeNode
+	{
+		public int val;
+		public TreeNode? left;
+		public TreeNode? right;
+
+		public TreeNode(int val = 0, TreeNode? left = null, TreeNode? right = null)
+		{
+			this.val = val;
+			this.left = left;
+			this.right = right;
+		}
+
+		public override string ToString()
+		{
+			return val.ToString();
+		}
+	}
+}
---
+++ tests/MyNihongo.Leetcode.Tests/Solutions/Solution108Should.cs
@@ -0,0 +1,62 @@
+﻿namespace MyNihongo.Leetcode.Tests.Solutions;
+
+public sealed class Solution108Should
+{
+	[Fact]
+	public void Case1()
+	{
+		int[] input = { -10, -3, 0, 5, 9 };
+		int?[] expected = { 0, -3, 9, -10, null, 5 };
+
+		var root = Solution108.SortedArrayToBST(input);
+		var result = GetList(root);
+
+		result
+			.Should()
+			.BeEquivalentTo(expected, x => x.WithStrictOrdering());
+	}
+
+	[Fact]
+	public void Case2()
+	{
+		int[] input = { 1, 3 };
+		int?[] expected = { 3, 1 };
+
+		var root = Solution108.SortedArrayToBST(input);
+		var result = GetList(root);
+
+		result
+			.Should()
+			.BeEquivalentTo(expected, x => x.WithStrictOrdering());
+	}
+
+	private static IEnumerable<int?> GetList(Solution108.TreeNode? root)
+	{
+		if (root == null)
+			return Enumerable.Empty<int?>();
+
+		var list = new List<int?>();
+		var queue = new Queue<Solution108.TreeNode?>();
+		queue.Enqueue(root);
+
+		while (queue.Count > 0)
+		{
+			for (int i = 0, count = queue.Count; i < count; i++)
+			{
+				var node = queue.Dequeue();
+				list.Add(node?.val);
+
+				if (node == null)
+					continue;
+
+				if (node.left == null && node.right == null)
+					continue;
+
+				queue.Enqueue(node.right);
+				queue.Enqueue(node.left);
+			}
+		}
+
+		return list;
+	}
+}
