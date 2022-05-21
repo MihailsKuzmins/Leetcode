35d28fa0c09b9813c63fb5dfa5ad89bae15d4569
ミシャ <47413092+MihailsKuzmins@users.noreply.github.com>
2022-05-21 10:28:14 +0900

    solution136

---
+++ src/Solutions/Solution136.cs
@@ -0,0 +1,17 @@
+﻿namespace MyNihongo.Leetcode.Solutions;
+
+/// <summary>
+/// Given a <b>non-empty</b> array of integers nums, every element appears <b>TWICE</b> except for one. Find that single one.<br/>
+/// You must implement a solution with a linear runtime complexity and use only constant extra space.
+/// </summary>
+public sealed class Solution136
+{
+	public static int SingleNumber(int[] nums)
+	{
+		var num = 0;
+		for (var i = 0; i < nums.Length; i++)
+			num ^= nums[i];
+
+		return num;
+	}
+}
