139d2d889f9017c63a2573c080b48d98915c0340
ミシャ <47413092+MihailsKuzmins@users.noreply.github.com>
2022-05-22 10:37:40 +0900

    solution144

---
+++ src/Solutions/Solution144.cs
@@ -0,0 +1,50 @@
+﻿// ReSharper disable InconsistentNaming
+namespace MyNihongo.Leetcode.Solutions;
+
+/// <summary>
+/// Given the root of a binary tree, return the preorder traversal of its nodes' values.
+/// </summary>
+public sealed class Solution144
+{
+	public static IList<int> PreorderTraversal(TreeNode? root)
+	{
+		if (root == null)
+			return Array.Empty<int>();
+
+		var stack = new Stack<TreeNode>();
+		stack.Push(root);
+
+		var list = new List<int>(1);
+		while (stack.Count != 0)
+		{
+			var node = stack.Pop();
+			list.Add(node.val);
+
+			if (node.right != null)
+				stack.Push(node.right);
+			if (node.left != null)
+				stack.Push(node.left);
+		}
+
+		return list;
+	}
+
+	public sealed class TreeNode
+	{
+		public int val;
+		public TreeNode? left;
+		public TreeNode? right;
+
+		public TreeNode(int val = 0, TreeNode? left = null, TreeNode? right = null)
+		{
+			this.val = val;
+			this.left = left;
+			this.right = right;
+		}
+
+		public override string ToString()
+		{
+			return val.ToString();
+		}
+	}
+}
---
+++ tests/MyNihongo.Leetcode.Tests/Solutions/Solution144Should.cs
@@ -0,0 +1,59 @@
+﻿namespace MyNihongo.Leetcode.Tests.Solutions;
+
+public sealed class Solution144Should
+{
+	[Fact]
+	public void Case1()
+	{
+		Solution144.TreeNode right1 = new(3), right = new(2, right1),
+			item = new(1, right: right);
+
+		var expected = new [] { 1, 2, 3 };
+
+		var result = Solution144.PreorderTraversal(item);
+
+		result
+			.Should()
+			.BeEquivalentTo(expected, x => x.WithStrictOrdering());
+	}
+
+	[Fact]
+	public void Case2()
+	{
+		var result = Solution144.PreorderTraversal(null);
+
+		result
+			.Should()
+			.BeEmpty();
+	}
+
+	[Fact]
+	public void Case3()
+	{
+		Solution144.TreeNode item = new(1);
+
+		var expected = new[] { 1 };
+
+		var result = Solution144.PreorderTraversal(item);
+
+		result
+			.Should()
+			.BeEquivalentTo(expected, x => x.WithStrictOrdering());
+	}
+
+	[Fact]
+	public void Case4()
+	{
+		Solution144.TreeNode left = new(1),
+			right = new(2),
+			item = new(3, left, right);
+
+		var expected = new[] { 3, 1, 2 };
+
+		var result = Solution144.PreorderTraversal(item);
+
+		result
+			.Should()
+			.BeEquivalentTo(expected, x => x.WithStrictOrdering());
+	}
+}
