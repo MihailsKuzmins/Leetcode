40af39051c2c3116a53872d3581438d9152dd27f
ミシャ <47413092+MihailsKuzmins@users.noreply.github.com>
2022-05-23 18:12:54 +0900

    solution145

---
+++ src/Solutions/Solution145.cs
@@ -0,0 +1,54 @@
+﻿// ReSharper disable InconsistentNaming
+namespace MyNihongo.Leetcode.Solutions;
+
+/// <summary>
+/// Given the root of a binary tree, return the postorder traversal of its nodes' values.
+/// </summary>
+public sealed class Solution145
+{
+	public static IList<int> PostorderTraversal(TreeNode? root)
+	{
+		if (root == null)
+			return Array.Empty<int>();
+
+		var temp = root;
+		var visited = new HashSet<TreeNode>();
+		var list = new List<int>();
+
+		while (temp != null && !visited.Contains(temp))
+		{
+			if (temp.left != null && !visited.Contains(temp.left))
+				temp = temp.left;
+			else if (temp.right != null && !visited.Contains(temp.right))
+				temp = temp.right;
+			else
+			{
+				list.Add(temp.val);
+
+				visited.Add(temp);
+				temp = root;
+			}
+		}
+
+		return list;
+	}
+
+	public sealed class TreeNode
+	{
+		public int val;
+		public TreeNode? left;
+		public TreeNode? right;
+
+		public TreeNode(int val = 0, TreeNode? left = null, TreeNode? right = null)
+		{
+			this.val = val;
+			this.left = left;
+			this.right = right;
+		}
+
+		public override string ToString()
+		{
+			return val.ToString();
+		}
+	}
+}
