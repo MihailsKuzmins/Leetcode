cdc74e29140f310a46354130798193117464addc
ミシャ <47413092+MihailsKuzmins@users.noreply.github.com>
2022-05-26 13:06:07 +0900

    solution160

---
+++ src/Solutions/Solution160.cs
@@ -0,0 +1,48 @@
+﻿// ReSharper disable InconsistentNaming
+namespace MyNihongo.Leetcode.Solutions;
+
+/// <summary>
+/// Given the heads of two singly linked-lists headA and headB, return the node at which the two lists intersect. If the two linked lists have no intersection at all, return null.
+/// </summary>
+public sealed class Solution160
+{
+	public static ListNode? GetIntersectionNode(ListNode? headA, ListNode? headB)
+	{
+		if (headA == null || headB == null)
+			return null;
+
+		// The idea is to set the current pointer of one list to the start pointer of another list
+		// Therefore, it is guaranteed that they will meet at the intersection after the swap of pointers
+		// If there is no intersection both pointers will be null
+		ListNode? currA = headA, currB = headB;
+		while (currA != currB)
+		{
+			currA = currA != null
+				? currA.next
+				: headB;
+
+			currB = currB != null
+				? currB.next
+				: headA;
+		}
+
+		return currA;
+	}
+
+	public sealed class ListNode
+	{
+		public int val;
+		public ListNode? next;
+
+		public ListNode(int val = 0, ListNode? next = null)
+		{
+			this.val = val;
+			this.next = next;
+		}
+
+		public override string ToString()
+		{
+			return val.ToString();
+		}
+	}
+}
---
+++ tests/MyNihongo.Leetcode.Tests/Solutions/Solution160Should.cs
@@ -0,0 +1,45 @@
+﻿namespace MyNihongo.Leetcode.Tests.Solutions;
+
+public sealed class Solution160Should
+{
+	[Fact]
+	public void Case1()
+	{
+		Solution160.ListNode c1 = new(5), c2 = new(4, c1), c3 = new(8, c2),
+			a1 = new(1, c3), a2 = new(4, a1),
+			b1 = new(1, c3), b2 = new(6, b1), b3 = new(5, b2);
+
+		var result = Solution160.GetIntersectionNode(a2, b3);
+
+		result
+			.Should()
+			.Be(c3);
+	}
+
+	[Fact]
+	public void Case2()
+	{
+		Solution160.ListNode c1 = new(2), c2 = new(4, c1),
+			a1 = new(1, c2), a2 = new(9, a1), a3 = new(1, a2),
+			b1 = new(3, c2);
+
+		var result = Solution160.GetIntersectionNode(a3, b1);
+
+		result
+			.Should()
+			.Be(c2);
+	}
+
+	[Fact]
+	public void Case3()
+	{
+		Solution160.ListNode a1 = new(4), a2 = new(6, a1), a3 = new(2, a2),
+			b1 = new(5), b2 = new(1, b1);
+
+		var result = Solution160.GetIntersectionNode(a3, b2);
+
+		result
+			.Should()
+			.BeNull();
+	}
+}
