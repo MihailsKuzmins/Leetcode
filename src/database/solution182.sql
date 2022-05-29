4cd60b5aa63f66f5ca7c0bf0701a7479aafbc6cb
ミシャ <47413092+MihailsKuzmins@users.noreply.github.com>
2022-05-29 12:54:00 +0900

    solution182

---
+++ src/database/solution182.sql
@@ -0,0 +1,21 @@
+/*
+Table Person
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| email       | varchar |
++-------------+---------+
+id is the primary key column for this table.
+Each row of this table contains an email. The emails will not contain uppercase letters.
+
+Write an SQL query to report all the duplicate emails.
+*/
+SELECT
+	email AS Email
+FROM
+	Person
+GROUP BY
+	email
+HAVING
+	COUNT(email) > 1
