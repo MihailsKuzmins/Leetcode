3ecb9a3e476343d2a558052b9153abb1b8ac06b3
ミシャ <47413092+MihailsKuzmins@users.noreply.github.com>
2022-06-02 23:22:37 +0900

    solution202

---
+++ src/algorithms_concurrency/Solutions/Solution202.cs
@@ -0,0 +1,37 @@
+﻿namespace MyNihongo.Leetcode.Solutions;
+
+/// <summary>
+/// Write an algorithm to determine if a number n is happy.<br/>
+/// A <b>happy number</b> is a number defined by the following process:<br/>
+/// - Starting with any positive integer, replace the number by the sum of the squares of its digits.<br/>
+/// - Repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1.<br/>
+/// - Those numbers for which this process ends in 1 are happy.<br/><br/>
+/// Return true if n is a happy number, and false if not.
+/// </summary>
+public sealed class Solution202
+{
+	public static bool IsHappy(int n)
+	{
+		var set = new HashSet<int>();
+
+		while (n != 1)
+		{
+			if (set.Contains(n))
+				return false;
+
+			set.Add(n);
+			var newN = 0;
+
+			while (n > 0)
+			{
+				var num = n % 10;
+				newN += num * num;
+				n /= 10;
+			}
+
+			n = newN;
+		}
+
+		return true;
+	}
+}
