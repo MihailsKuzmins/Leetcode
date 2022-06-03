9398d2459745f029aabc720846fd9041d310bcce
ミシャ <47413092+MihailsKuzmins@users.noreply.github.com>
2022-06-03 13:15:02 +0900

    solution206

---
+++ src/algorithms_concurrency/Solutions/Solution206.cs
@@ -0,0 +1,26 @@
+﻿namespace MyNihongo.Leetcode.Solutions;
+
+/// <summary>
+/// Given the head of a singly linked list, reverse the list, and return the reversed list.
+/// </summary>
+public sealed class Solution206
+{
+	public static ListNode? ReverseList(ListNode? head)
+	{
+		if (head == null)
+			return null;
+
+		ListNode? prev = head, curr = head.next;
+		prev.next = null;
+		while (curr != null)
+		{
+			var tmp = curr.next;
+			curr.next = prev;
+
+			prev = curr;
+			curr = tmp;
+		}
+
+		return prev;
+	}
+}
