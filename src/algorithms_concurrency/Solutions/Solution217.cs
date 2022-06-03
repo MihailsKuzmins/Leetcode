4f470e4f1aac2e6b8f69115769b978c102f46438
ミシャ <47413092+MihailsKuzmins@users.noreply.github.com>
2022-06-03 15:05:27 +0900

    solution217

---
+++ src/algorithms_concurrency/Solutions/Solution217.cs
@@ -0,0 +1,19 @@
+﻿namespace MyNihongo.Leetcode.Solutions;
+
+/// <summary>
+/// Given an integer array nums, return true if any value appears <b>at least twice</b> in the array, and return false if every element is distinct.
+/// </summary>
+public sealed class Solution217
+{
+	public static bool ContainsDuplicate(int[] nums)
+	{
+		var set = new HashSet<int>();
+		for (var i = 0; i < nums.Length; i++)
+		{
+			if (!set.Add(nums[i]))
+				return true;
+		}
+
+		return false;
+	}
+}
